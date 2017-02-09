c000624f39b1e8e2f0f3ef8b7acf49179a13e8c3
alexhubbard89 <alexhubbard89@gmail.com>
2017-02-09 14:04:23 -0500

    update requirements doc

--- python/tally_toolkit.py
+++ python/tally_toolkit.py
@@ -20,29 +20,15 @@ from unidecode import unidecode
 # urlparse.uses_netloc.append("postgres")
 # url = urlparse.urlparse(os.environ["HEROKU_POSTGRESQL_BROWN_URL"])
     
-# def open_connection():
-#     connection = psycopg2.connect(
-#         database=url.path[1:],
-#         user=url.username,
-#         password=url.password,
-#         host=url.hostname,
-#         port=url.port
-#         )
-#     return connection
-
-urlparse.uses_netloc.append("postgres")
-creds = pd.read_json('/Users/Alexanderhubbard/Documents/projects/reps_app/app/db_creds.json').loc[0,'creds']
-
 def open_connection():
     connection = psycopg2.connect(
-        database=creds['database'],
-        user=creds['user'],
-        password=creds['password'],
-        host=creds['host'],
-        port=creds['port']
+        database=url.path[1:],
+        user=url.username,
+        password=url.password,
+        host=url.hostname,
+        port=url.port
         )
     return connection
-
 
 class user_info(object):
     """
