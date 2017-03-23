c95bb79c48cf64fb06a1ba9b91e9731432e668d5
alexhubbard89 <alexhubbard89@gmail.com>
2017-03-23 10:06:37 -0400

    fix tracker collection

--- python/missing_sponsorship_114.py
+++ python/missing_sponsorship_114.py
@@ -1,7 +1,9 @@
 import imp
 tally_toolkit = imp.load_source('module', './python/tally_toolkit.py')
 
-sponsorship_data = tally_toolkit.sponsorship_collection()
-sponsorship_data.congress_search = 114
+leg_collection = tally_toolkit.collect_legislation()
+leg_collection.congress_search = 114
+tally_toolkit.collect_legislation.legislation_info_by_congress(leg_collection)
+tally_toolkit.collect_legislation.legislation_to_sql(leg_collection)
 
-tally_toolkit.sponsorship_collection.collect_sponsorship(sponsorship_data)
+print 'done! 114'
